b22801ae15a383b356a8db0a226d756188215b88
eqan <eqan.ahmad123@gmail.com>
2021-06-11 16:01:24 +0500

    Total program revamp

---
+++ Main.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Word_Search
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.find_btn = new System.Windows.Forms.Button();
+            this.cancel_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.algorithm_menu = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // find_btn
+            // 
+            this.find_btn.Location = new System.Drawing.Point(270, 35);
+            this.find_btn.Name = "find_btn";
+            this.find_btn.Size = new System.Drawing.Size(75, 23);
+            this.find_btn.TabIndex = 0;
+            this.find_btn.Text = "Find Next";
+            this.find_btn.UseVisualStyleBackColor = true;
+            this.find_btn.Click += new System.EventHandler(this.findbutton_click);
+            // 
+            // cancel_btn
+            // 
+            this.cancel_btn.Location = new System.Drawing.Point(270, 64);
+            this.cancel_btn.Name = "cancel_btn";
+            this.cancel_btn.Size = new System.Drawing.Size(75, 23);
+            this.cancel_btn.TabIndex = 1;
+            this.cancel_btn.Text = "Cancel";
+            this.cancel_btn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Find What:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(39, 70);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Match whole word only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(39, 93);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(82, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Match case";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 130);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(329, 397);
+            this.textBox2.TabIndex = 6;
+            // 
+            // algorithm_menu
+            // 
+            this.algorithm_menu.FormattingEnabled = true;
+            this.algorithm_menu.Items.AddRange(new object[] {
+            "Brute Force",
+            "Rabin Karp",
+            "KMP"});
+            this.algorithm_menu.Location = new System.Drawing.Point(101, 12);
+            this.algorithm_menu.Name = "algorithm_menu";
+            this.algorithm_menu.Size = new System.Drawing.Size(130, 21);
+            this.algorithm_menu.TabIndex = 7;
+            this.algorithm_menu.Text = "Select Algorithm";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 539);
+            this.Controls.Add(this.algorithm_menu);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancel_btn);
+            this.Controls.Add(this.find_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button find_btn;
+        private System.Windows.Forms.Button cancel_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox algorithm_menu;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+namespace Word_Search
+{
+    public partial class Main : Form
+    {
+        string fileString;
+        bool checker = false;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void findbutton_click(object sender, EventArgs e)
+        {
+            switch(algorithm_menu.SelectedIndex)
+            {
+                case 0:
+                    show_bruteforce_results();
+                    break;
+                case 1:
+                    show_rabinkarp_results();
+                    break;
+                case 2:
+                    show_kmp_results();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void show_bruteforce_results()
+        {
+            String textFile = "exp1.txt";
+            String pattern = textBox1.Text;
+            int ptnLength = pattern.Length;
+            int readLength;
+            int row = 1;
+            textBox2.Text = "---RESULT BY BRUTE FORCE---\r\n\r\n";
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                while ((fileString = file.ReadLine()) != null)
+                {
+                    readLength = fileString.Length;
+                    if (BruteForce(fileString, pattern, readLength, ptnLength) > 0)
+                    {
+                        textBox2.Text += "\r\nRow Number: " + row.ToString() + "\r\n\r\n";
+                    }
+                    row++;
+                }
+                file.Close();
+            }
+        }
+
+
+        private void show_kmp_results()
+        {
+            String textFile = "exp1.txt";
+            String pattern = textBox1.Text;
+            int ptnLength = pattern.Length;
+            int readLength;
+            int row = 1;
+            textBox2.Text = "---RESULT BY KMP---\r\n\r\n";
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                while ((fileString = file.ReadLine()) != null)
+                {
+                    readLength = fileString.Length;
+                    if (KMP(fileString, pattern, readLength, ptnLength) > 0)
+                    {
+                        textBox2.Text += "\r\nRow Number: " + row.ToString() + "\r\n\r\n";
+                    }
+                    row++;
+                }
+                file.Close();
+            }
+        }
+
+        private void show_rabinkarp_results()
+        {
+            String textFile = "exp1.txt";
+            String pattern = textBox1.Text;
+            int ptnLength = pattern.Length;
+            int readLength;
+            int row = 1;
+            textBox2.Text = "---RESULT BY RABINKARP FORCE---\r\n\r\n";
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                while ((fileString = file.ReadLine()) != null)
+                {
+                    readLength = fileString.Length;
+                    if (RabinKarp(fileString, pattern) > 0)
+                    {
+                        textBox2.Text += "\r\nRow Number: " + row.ToString() + "\r\n\r\n";
+                    }
+                    row++;
+                }
+                file.Close();
+            }
+        }
+
+        public int RabinKarp(string txt, string pat)
+        {
+            String tmp;
+            int d = 256;
+            int prime = 7;
+            int M = pat.Length;
+            int N = txt.Length;
+            int i, j;
+            int p = 0;
+            int t = 0;
+            int h = 1;
+            for (i = 0; i < M - 1; i++)
+            {
+                h = (h * d) % prime;
+            }
+            for (i = 0; i < M; i++)
+            {
+                p = (d * p + pat[i]) % prime;
+                t = (d * t + txt[i]) % prime;
+            }
+            for (i = 0; i <= N - M; i++)
+            {
+                tmp = "";
+                if (p == t)
+                {
+                    for (j = 0; j < M; j++)
+                    {
+                        if (txt[i + j] != pat[j])
+                            break;
+                    }
+                    if (j == M)
+                    {
+                        int x = i;
+                        while (txt[x] != ' ')
+                        {
+                            x--;
+                            if (x < 0)
+                            {
+                                break;
+                            }
+                        }
+                        x++;
+                        while (txt[x] != ' ')
+                        {
+                            tmp += txt[x];
+                            x++;
+                            if (x == N)
+                            {
+                                break;
+                            }
+                        }
+                        if (checkBox1.Checked)
+                        {
+                            if (tmp != pat && tmp.ToLower() != pat.ToLower())
+                            {
+                                return -1;
+                            }
+                        }
+                        textBox2.Text += "Word Found : " + tmp;
+                        textBox2.Text += "\r\nColumn Number : " + i.ToString() + "\r\n";
+                        checker = true;
+                    }
+                }
+                if (i < N - M)
+                {
+                    t = (d * (t - txt[i] * h) + txt[i + M]) % prime;
+                    if (t < 0)
+                        t = (t + prime);
+                }
+            }
+            if (checker)
+            {
+                checker = false;
+                return 1;
+            }
+            return -1;
+        }
+
+
+
+
+        public int KMP(string Text, string Pattern, int N, int M)
+        {
+            String tmp = "";
+            int[] prefixArray = new int[M];
+            int matches = 0;
+            prefixArray[0] = matches;
+            for (int i = 1; i < M; i++)
+            {
+                if (Pattern[i] == Pattern[matches])
+                {
+                    prefixArray[i] = ++matches;
+                }
+                else
+                {
+                    matches = 0;
+                    prefixArray[i] = matches;
+                }
+            }
+            int j = 0;
+            for (int i = 0; i <= N - M; i++)
+            {
+                tmp = "";
+                if (Char.ToLower(Text[i]) == Char.ToLower(Pattern[0]))
+                {
+                    for (j = 0; j < M; j++)
+                    {
+                        if (checkBox2.Checked)
+                        {
+                            if (Text[i + j] != Pattern[j])
+                            {
+                                i = i + j - prefixArray[j] - 1;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (Char.ToLower(Text[i + j]) != Pattern[j] && Char.ToUpper(Text[i + j]) != Pattern[j])
+                            {
+                                i = i + j - prefixArray[j] - 1;
+                                break;
+                            }
+                        }
+                    }
+                    if (j == M)
+                    {
+                        int x = i;
+                        while (Text[x] != ' ')
+                        {
+                            x--;
+                            if (x < 0)
+                            {
+                                break;
+                            }
+                        }
+                        x++;
+                        while (Text[x] != ' ')
+                        {
+                            tmp += Text[x];
+                            x++;
+                            if (x == N)
+                            {
+                                break;
+                            }
+                        }
+                        if (checkBox1.Checked)
+                        {
+                            if (tmp != Pattern && tmp.ToLower() != Pattern.ToLower())
+                            {
+                                return -1;
+                            }
+                        }
+                        textBox2.Text += "Word Found : " + tmp;
+                        textBox2.Text += "\r\nColumn Number : " + i.ToString() + "\r\n";
+                        checker = true;
+                        i++;
+                    }
+                }
+            }
+            if (checker)
+            {
+                checker = false;
+                return 1;
+            }
+            return -1;
+        }
+
+
+
+        public int BruteForce(string Text, string Pattern, int N, int M)
+        {
+            String tmp = "";
+            for (int i = 0; i <= N - M; i++)
+            {
+                tmp = "";
+                int j;
+                for (j = 0; j < M; j++)
+                {
+                    if (checkBox2.Checked)
+                    {
+                        if (Text[i + j] != Pattern[j]) // Not Pattern[i]
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (Char.ToUpper(Text[i + j]) != Pattern[j] && Char.ToLower(Text[i + j]) != Pattern[j]) // Not Pattern[i]
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (j == M)
+                {
+                    int x = i;
+                    while (Text[x] != ' ')
+                    {
+                        x--;
+                        if (x < 0)
+                        {
+                            break;
+                        }
+                    }
+                    x++;
+                    while (Text[x] != ' ')
+                    {
+                        tmp += Text[x];
+                        x++;
+                        if (x == N)
+                        {
+                            break;
+                        }
+                    }
+                    if (checkBox1.Checked)
+                    {
+                        if (tmp != Pattern && tmp.ToLower() != Pattern.ToLower())
+                        {
+                            return -1;
+                        }
+                    }
+                    textBox2.Text += "Word Found : " + tmp;
+                    textBox2.Text += "\r\nColumn Number : " + i.ToString() + "\r\n\r\n";
+                    checker = true;
+                    i++;
+                }
+            }
+            if (checker)
+            {
+                checker = false;
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
